c2cb8c26b27a727f50202697badd9b069e540d7e
Jasmeet Singh <jasmsing@ca.ibm.com>
2017-01-04 15:12:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ EJS.close = "</ejs>";
 
 var host = process.env.PORT ? '0.0.0.0' : 'localhost';
 var port = (process.env.PORT || 3456);
-console.log('App Name...');
-var appName = process.env.VCAP_APPLICATION ? JSON.parse(process.env.VCAP_APPLICATION).name : 'localhost';
-console.log(appName);
 var url = require('url').format({hostname: host, port: port, protocol: 'http'});
 
 var app = express();
